f30c55977802342bcf8b87c50fa2de96d7a3ed0d
crptmem <corrupt@kijy.de>
2024-02-25 16:11:15 +0300

    feat: multiple obtaining methods

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
+use std::error::Error;
+
+use axum::body::Body;
 use axum::{
     response::IntoResponse, routing::get, Router
 };
 
+use axum::http::StatusCode;
+use tokio_util::io::ReaderStream;
 use axum::extract::Path;
 use chrono::Local;
 use fern::colors::{Color, ColoredLevelConfig};
@@ -11,13 +16,19 @@ use reqwest;
 use clap::Parser;
 
 /// Skin obtaining methods
-#[derive(clap::ValueEnum, Clone, Debug)]
+#[derive(clap::ValueEnum, Clone, Debug, PartialEq, Eq)]
 enum ObtainingMethod {
     /// Get skins and cloaks from Minecraft servers
     License,
 
     /// Use local skins and cloaks
-    Local
+    Local,
+
+    /// Use Minecraft servers to obtain only skins
+    SkinsLicense,
+
+    /// Use Minecraft servers to obtain only cloaks
+    CloaksLicense
 }
 
 /// Program arguments
@@ -79,13 +90,51 @@ async fn root() -> &'static str {
 
 /// Skin route handler
 async fn skin(Path(username): Path<String>) -> impl IntoResponse {
-    let result = reqwest::get(format!("https://mc-heads.net/download/{}", username)).await.unwrap();
-    result.bytes().await.unwrap()
+    let args = Args::parse();
+    if username.contains("../") {
+        return err("forbidden username provided".to_string());
+    }
+    if args.obtaining_method == ObtainingMethod::License || args.obtaining_method == ObtainingMethod::SkinsLicense {
+        let result = reqwest::get(format!("https://mc-heads.net/download/{}", username)).await.unwrap();
+        info!("Sending request to {}", format!("https://mc-heads.net/download/{}", username));
+        Ok(Body::from(result.bytes().await.unwrap()))
+    } else {
+        let path = if username.contains(".png") { format!("assets/skins/{}", username) } else { format!("assets/skins/{}.png", username) };
+        info!("Reading {}", path);
+        let file = match tokio::fs::File::open(path).await {
+            Ok(file) => file,
+            Err(err) => return Err(format!("File not found: {}", err)),
+        };
+        // convert the `AsyncRead` into a `Stream`
+        let stream = ReaderStream::new(file);
+        
+        // convert the `Stream` into an `axum::body::HttpBody`
+        Ok(Body::from_stream(stream))
+    }
 }
 
 /// Cloak route handler
 async fn cloak(Path(username): Path<String>) -> impl IntoResponse {
     // NOT IMPLEMENTED
-    let result = reqwest::get(format!("https://mc-heads.net/download/{}", username)).await.unwrap();
-    result.bytes().await.unwrap()
+    let args = Args::parse();
+    if username.contains("../") {
+        return Err("forbidden username provided".to_string());
+    }
+    if args.obtaining_method == ObtainingMethod::License || args.obtaining_method == ObtainingMethod::SkinsLicense {
+        let result = reqwest::get(format!("https://mc-heads.net/download/{}", username)).await.unwrap();
+        info!("Sending request to {}", format!("https://mc-heads.net/download/{}", username));
+        Ok(Body::from(result.bytes().await.unwrap()))
+    } else {
+        let path = if username.contains(".png") { format!("assets/skins/{}", username) } else { format!("assets/skins/{}.png", username) };
+        info!("Reading {}", path);
+        let file = match tokio::fs::File::open(path).await {
+            Ok(file) => file,
+            Err(err) => return Err(format!("File not found: {}", err)),
+        };
+        // convert the `AsyncRead` into a `Stream`
+        let stream = ReaderStream::new(file);
+        
+        // convert the `Stream` into an `axum::body::HttpBody`
+        Ok(Body::from_stream(stream))
+    }
 }
